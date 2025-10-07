8290e14df5b05bb463de6d3cceaa796f8f33f662
Marcelo Alves <alvesmarcelocf@proton.me>
2025-10-07 17:52:57 -0300

    feat: append extra hosts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,7 +47,28 @@
   ];
   
   # Define your hostname.
-  networking.hostName = "starscream"; 
+  networking.hostName = "starscream";
+
+  # Add additional entries on hosts file
+  networking.extraHosts = ''
+# AKS private clusters
+10.33.0.10            rnn-new-omni-kubernetes-dev-v8z8h6o4.privatelink.eastus.azmk8s.io
+10.34.0.10            rnn-sup-kubernetes-nonprod-fesad9t9.privatelink.eastus.azmk8s.io
+10.34.128.100         rnn-sup-kubernetes-prod-x2b7b1bp.privatelink.brazilsouth.azmk8s.io
+10.35.0.99            rnn-new-omni-kubernetes-prod-izplsxyy.privatelink.brazilsouth.azmk8s.io
+10.38.0.10            rnn-log-lyt-kubernetes-dev-89dqigjy.privatelink.eastus.azmk8s.io
+10.38.128.10          rnn-log-lyt-kubernetes-hml-m15eu6h5.privatelink.eastus.azmk8s.io
+10.39.0.4             rnn-log-lyt-kubernetes-prod-u5uoopb1.privatelink.brazilsouth.azmk8s.io
+
+# Custom entries
+10.206.10.93          gitlab-corp.lojasrenner.io
+10.34.0.121           n8n-sre-nonprod.lojasrenner.io
+10.34.128.121         grafana-azure-prod.lojasrenner.io
+10.34.0.121           percona-pmm-azure-nonprod.lojasrenner.io
+10.34.0.121           elasticsearch-azure-nonprod.lojasrenner.io
+10.34.128.121         elasticsearch-azure-prod.lojasrenner.io
+10.34.128.121         sonarqube-azure-prod.lojasrenner.io
+  '';
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
