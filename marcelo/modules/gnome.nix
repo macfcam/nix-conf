11b3b55e5d61a584794cc47079c6208b33954149
Marcelo Alves <alvesmarcelocf@proton.me>
2025-10-20 08:59:07 -0300

    feat: add gnome-terminal and settings

--- marcelo/modules/gnome.nix
+++ marcelo/modules/gnome.nix
@@ -9,9 +9,10 @@
     gnomeExtensions.blur-my-shell
     gnomeExtensions.clipboard-indicator
     gnomeExtensions.dash-to-dock
-    gnome-tweaks
-    gnome-shell-extensions
     gnome-extension-manager
+    gnome-shell-extensions
+    gnome-terminal
+    gnome-tweaks
     nautilus
     resources
   ];
@@ -30,6 +31,62 @@
       ];
     };
     
+    # Gnome Terminal
+    "org/gnome/terminal/legacy" = {
+      default-show-menubar = false;
+      schema-version = 3;
+      theme-variant = "default";
+    };
+
+    "org/gnome/terminal/legacy/profiles:" = {
+      default = "95894cfd-82f7-430d-af6e-84d168bc34f5";
+      list = [ "95894cfd-82f7-430d-af6e-84d168bc34f5" ];
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:95894cfd-82f7-430d-af6e-84d168bc34f5" = {
+      audible-bell = true;
+      background-color = "#1e1e2e";
+      backspace-binding = "ascii-delete";
+      bold-color-same-as-fg = true;
+      cursor-blink-mode = "system";
+      cursor-colors-set = false;
+      cursor-shape = "block";
+      custom-command = "fish";
+      default-size-columns = 130;
+      default-size-rows = 40;
+      delete-binding = "delete-sequence";
+      font = "FantasqueSansM Nerd Font Mono 12";
+      foreground-color = "#cdd6f4";
+      highlight-colors-set = false;
+      login-shell = false;
+      palette = [ 
+        "#45475a"
+        "#f38ba8"
+        "#a6e3a1"
+        "#f9e2af"
+        "#89b4fa"
+        "#f5c2e7"
+        "#94e2d5"
+        "#a6adc8"
+        "#585b70"
+        "#f37799"
+        "#89d88b"
+        "#ebd391"
+        "#74a8fc"
+        "#f2aede"
+        "#6bd7ca"
+        "#bac2de" 
+      ];
+      scroll-on-output = true;
+      scrollback-lines = 10000;
+      scrollback-unlimited = true;
+      scrollbar-policy = "never";
+      use-custom-command = true;
+      use-system-font = false;
+      use-theme-colors = false;
+      visible-name = "Catppuccin Mocha";
+    };
+
     # Dash to Dock settings - not working at this moment
     # "org/gnome/shell/extensions/dash-to-dock" = {
     #   activate-single-window = true;
