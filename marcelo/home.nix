3b5f8999cf84ca7be52a200c8a1c884e0cbb9c9c
Marcelo Alves <alvesmarcelocf@proton.me>
2025-10-24 15:33:46 -0300

    fix: set ANSIBLE_SSH_ARGS variable

--- marcelo/home.nix
+++ marcelo/home.nix
@@ -8,12 +8,12 @@ in
   home.username = "marcelo";
   home.homeDirectory = "/home/marcelo";
   home.stateVersion = "25.05";
-
   home.sessionVariables = {
+    ANSIBLE_SSH_ARGS = "-o ControlMaster=no";
     EDITOR = "nvim";
+    MOZ_ENABLE_WAYLAND = "1";
     NIXOS_OZONE_WL = "1";
     TG_DOWNLOAD_DIR = "$HOME/.terragrunt-cache";
-    MOZ_ENABLE_WAYLAND = "1";
   };
 
   imports = [
