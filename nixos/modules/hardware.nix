6224c785a108a3c36724242eece0293968fe9d6e
Marcelo Alves <alvesmarcelocf@proton.me>
2025-10-27 21:58:46 -0300

    refactor: duplicated nixpkgs.config.allowUnfree

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -4,7 +4,7 @@
   # Hardware related settings
   hardware = {
     enableAllFirmware = true;
-    cpu.amd.updateMicrocode = true;       # Enable AMD microcode updates
+    cpu.amd.updateMicrocode = true; # Enable AMD microcode updates
 
     bluetooth = {
       enable = true;
@@ -13,14 +13,10 @@
 
     graphics = {
       enable = true;
-      extraPackages = with pkgs; [ mesa libglvnd ];
-    };
-  };
-
-  # Nixpkgs related settings
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
+      extraPackages = with pkgs; [
+        mesa
+        libglvnd
+      ];
     };
   };
 }
