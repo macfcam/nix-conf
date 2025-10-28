2b78be54e8e5db13d7808b167da217a69bc2d123
Marcelo Alves <alvesmarcelocf@proton.me>
2025-10-28 10:50:13 -0300

    fix: minor docker changes

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -1,16 +1,26 @@
 { config, pkgs, ... }:
 {
   virtualisation = {
-    docker = { 
+    docker = {
       enable = true;
+      enableOnBoot = true;
+      autoPrune.enable = true;
+      daemon = {
+        settings = {
+          "experimental" = true;
+        };
+      };
     };
+
     libvirtd = {
       enable = true;
       qemu = {
-        package = (pkgs.qemu_kvm.override {
-          enableDocs = false;
-          cephSupport = false;
-        });
+        package = (
+          pkgs.qemu_kvm.override {
+            enableDocs = false;
+            cephSupport = false;
+          }
+        );
         runAsRoot = true;
         swtpm.enable = true;
       };
