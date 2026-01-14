d8198b9f18834c4e16dd48ce024ff52dcba84f5f
Marcelo Alves <alvesmarcelocf@proton.me>
2026-01-14 09:42:24 -0300

    feat: limit systemd-coredumps to 500M for preventing huge dumps

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -26,4 +26,20 @@
 
   # Configure console keymap
   console.keyMap = "us";
+
+  systemd = {
+    coredump = {
+      extraConfig = ''
+        # Limit individual dump to 500MB (enough for most debugging)
+        ProcessSizeMax=500M
+        ExternalSizeMax=500M
+
+        # Keep only 1GB total of dumps
+        MaxUse=1G
+
+        # Compress dumps to save space
+        Compress=yes
+      '';
+    };
+  };
 }
